26284e77fd80caf044fc4791adcb3c626cbb7696
Luis <100473510+carlos8788@users.noreply.github.com>
2024-01-28 09:36:20 -0300

    ej 3.4 ready

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
     { id: 1, name: "Arto Hellas", number: "040-123456" },
     { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
     { id: 3, name: "Dan Abramov", number: "12-43-234345" },
@@ -18,7 +18,18 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
     const { id } = req.params
     const person = persons.find(person => person.id === Number(id));
-    (person) ? res.json(person) : res.status(404).json({message: 'No such person'})
+    (person) ? res.json(person) : res.status(404).json({ message: 'No such person' })
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const { id } = req.params
+    const person = persons.find(person => person.id === Number(id));
+    if (person) {
+        persons = persons.filter(person => person.id !== Number(id));
+        return res.json({message: 'Person was deleted succesufully'})
+    }else{
+        return res.status(404).json({message: 'Person not found'})
+    }
 })
 
 app.get('/info', (req, res) => {
